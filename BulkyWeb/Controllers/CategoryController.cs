e644a2830e174e492ce5b956146c8d610f444eae
Βασιλης Κακιωνης <vasilhs_kakionis@yahoo.gr>
2023-10-17 22:31:57 +0300

    commit 1

---
+++ BulkyWeb/Controllers/CategoryController.cs
@@ -0,0 +1,24 @@
+﻿using BulkyWeb.Data;
+using BulkyWeb.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BulkyWeb.Controllers
+{
+    public class CategoryController : Controller
+    {
+        public ApplicationDbContext _db;
+        public CategoryController(ApplicationDbContext db)
+        {
+            
+            _db = db;
+            Console.WriteLine("Any String " + db);
+            var x = 0;
+        }
+        public IActionResult Index(ApplicationDbContext db)
+        {
+            var x = 1;
+            List<Category> objCategoryList = _db.Categories.ToList();
+            return View();
+        }
+    }
+}
